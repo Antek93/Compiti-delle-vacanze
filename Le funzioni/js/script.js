23e1e6e7fc1c450bf4126e80109690cb38770094
Antek93 <antonio.savino1993@gmail.com>
2023-01-10 11:06:00 +0100

    teoria aggiunta

--- Le funzioni/js/script.js
+++ Le funzioni/js/script.js
@@ -13,3 +13,36 @@ console.log('HI, I am java and I am working fine')
 function miaFunzione () {
     
 }
+
+// All'interno delle parentesi tonde: argomenti;
+
+// Gli argomenti ci possono essere se necessari, se non viene specificato si parla di funzione ANONIMA
+
+// Possono avere un nome qualunque, ma è preferibile utillizare un nome appropriato per anticipare il lavoro da farsi
+
+//Per restituire un valore si usa il keyword RETURN
+
+// keyword   //Nome funzione    //Argomenti
+function     mia2Funzione      (num1, num2) {
+    
+    const risultato = num1 + num2;
+
+    return risultato; //quando scriviamo RETURN la funzione TERMINA, tutto il codice dopo non viene eseguito.
+}
+
+//______________________________________________________________________________________________________________
+
+function sommaNumeri (num1, num2) { 
+    
+    const risultato = num1 + num2;
+
+    return risultato;
+}
+
+const somma = sommaNumeri (49, 2); //Definiti num1 e num2 e richiamata la funzione sommaNumeri, viene eseguita l'operazione della funzione
+
+console.log (somma); // darà il risultato corretto della funzione sommaNumeri, poichè è stata richiamata; (51)
+// console.log (risultato); Darebbe errore se lavorasse, perché risultato esiste solo nello scope della funzione;
+
+//_________________________________________________________________________________________________________________
+
