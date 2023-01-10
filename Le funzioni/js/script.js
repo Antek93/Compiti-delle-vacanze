1e819d12b9a94da0f8465daf2125d3776fadd336
Antek93 <antonio.savino1993@gmail.com>
2023-01-10 10:32:25 +0100

    teoria agg

--- Le funzioni/js/script.js
+++ Le funzioni/js/script.js
@@ -3,3 +3,13 @@ console.log('HI, I am java and I am working fine')
 // Le funzioni
 
 // Servono per riutilizzare un pezzo di codice piu volte 
+
+/* Come si scrive una funzione */
+
+// keyword: function;
+// nome funzione: si imposta il nome della funzione per poterla richiamare successivamente + ();
+// nelle parentesi graffe inseriamo il codice che vogliamo inserire;
+
+function miaFunzione () {
+    
+}
