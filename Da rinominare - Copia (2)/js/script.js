f190fcb7aa0abb0123e2b55d991a5ffe5434e8d1
Antek93 <antonio.savino1993@gmail.com>
2023-01-13 11:39:24 +0100

    aggiunte info

--- Da rinominare - Copia (2)/js/script.js
+++ Da rinominare - Copia (2)/js/script.js
@@ -1,1 +1,41 @@
-console.log('HI, I am java and I am working fine')
+console.log('HI, I am java and I am working fine');
+
+// _________________________________________________________________________________//
+
+
+// Arrow Function
+
+(/*Queste parentesi sostituiscono la keyword Function*/) => //codice
+
+/* 1.) Se non utilizziamo le parentesi graffe, il return è implicito;*/ !important;
+
+(/*Queste parentesi sostituiscono la keyword Function*/) => { } // o parentesi graffe se su piu righe
+
+/* 2.) Se utilizziamo le parentesi graffe, il return va specificato;*/ !important;
+
+// _________________________________________________________________________________//
+
+
+// _________________________________________________________________________________//
+
+
+// -- Possono essere nominate o anonime --
+
+const         funzione   =        () =>             1 + 1;
+
+// const + nome funzione = sintassi funzione     //esempio
+
+/* 3.) Se abbiamo bisogno del this conviene utilizzare una funzione ANOMINA*/ !important;
+
+// Utilizzando una function anonima, possiamo intercettare l'elemento del DOM che ha scatenato l'EventCounts, utilizzando il this 
+
+// _________________________________________________________________________________//
+
+
+
+
+
+
+
+
+
