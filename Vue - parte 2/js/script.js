c142d10ac88c0f516327470b137a781caecbcaeb
Antek93 <antonio.savino1993@gmail.com>
2023-01-23 10:06:17 +0100

    aggiunta estensione con vue

--- Vue - parte 2/js/script.js
+++ Vue - parte 2/js/script.js
@@ -1,21 +1,30 @@
 console.log('Hi, I am java and I am working fine');
 
-const { createApp } = Vue;
+// what's happening?
 
-createApp({
-    data() {
-        return {
-            greeting: 'Ciao Antonio',
+const { createApp } = Vue; // E' un deconstructuring della funzione create App
+
+
+// createApp si compone di:
+
+// Data and methods
+
+createApp({  // oggetto esterno
+
+    
+    data() {  // funzione                                  In data inseriamo tutte la variabili 
+        return { // ritorno di funzione
+            greeting: 'Ciao Antonio', //
             oggetto: {
                 esempio1: 'Ciao',
                 esempio2: 'Come va?'
             }
         }
     },
-    methods: {
+    methods: { // funzione
         functionExample: function () {
-            return  console.log('I am Vue and I am working properly');
+            return  console.log('I am Vue and I am working properly'); //ritorno di funzione
         },
         
     }
-}).mount('#app');
+}).mount('#app'); // collega l'oggetto createApp (con .mount) con l'html tramite il div con ID 'app'
