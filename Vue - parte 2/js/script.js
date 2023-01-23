f520bae636ca50455911f4243abfc5e1db5bb1c7
Antek93 <antonio.savino1993@gmail.com>
2023-01-23 09:40:23 +0100

    scaffolding

---
+++ Vue - parte 2/js/script.js
@@ -0,0 +1,21 @@
+console.log('Hi, I am java and I am working fine');
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            greeting: 'Ciao Antonio',
+            oggetto: {
+                esempio1: 'Ciao',
+                esempio2: 'Come va?'
+            }
+        }
+    },
+    methods: {
+        functionExample: function () {
+            return  console.log('I am Vue and I am working properly');
+        },
+        
+    }
+}).mount('#app');
